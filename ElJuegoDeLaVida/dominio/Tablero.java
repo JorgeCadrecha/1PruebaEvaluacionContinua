afc57d692b205fc48c43eecd106f4e9fae1b00a1
JorgeCadrecha <113858332+JorgeCadrecha@users.noreply.github.com>
2023-03-23 18:37:30 +0000

    Inicio

---
+++ ElJuegoDeLaVida/dominio/Tablero.java
@@ -0,0 +1,26 @@
+package ElJuegoDeLaVida.dominio;
+
+public class Tablero {
+    private static  int DIMENSION = 30;
+    private int[][] estadoActual;
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
+
+    public void leerEstadoActual(){
+
+
+    }
+
+    public void generarEstadoActualPorMontecarlo(){
+
+    }
+
+    public void transitarAlEstadoSiguiente(){
+
+    }
+
+    @Override
+    public String toString(){
+
+        return "";
+    }
+}
